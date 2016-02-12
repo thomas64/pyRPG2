5d264a9a2dcbbd254497b1fbda8e95820c19cb99
Thomas M. Hesselink <mail@t64.nl>
2016-02-12 02:04:07 +0100

    HeroBox class in partyscreen gemaakt.

--- screens/partyscreen.py
+++ screens/partyscreen.py
@@ -1,6 +1,7 @@
 
 """
 class: PartyScreen
+class: HeroBox
 """
 
 import pygame
@@ -34,9 +35,6 @@ class PartyScreen(object):
         self.background.fill(BACKGROUNDCOLOR)
         self.background = self.background.convert()
 
-        self._paint()
-        self._init_buttons()
-
         self.key_input = pygame.key.get_pressed()       # dit is voor de mousepress op een button.
 
         self.largefont = pygame.font.SysFont(FONT, LARGEFONTSIZE)
@@ -47,18 +45,8 @@ class PartyScreen(object):
         self.party = list(data.party.values())
         self.hc = self.party.index(cur_hero)
 
-    def _paint(self):
-        hero1_rect = pygame.draw.rect(self.background, LINECOLOR, (10,   10, 250, 98), 1)
-        hero2_rect = pygame.draw.rect(self.background, LINECOLOR, (270,  10, 250, 98), 1)
-        hero3_rect = pygame.draw.rect(self.background, LINECOLOR, (530,  10, 250, 98), 1)
-        hero4_rect = pygame.draw.rect(self.background, LINECOLOR, (790,  10, 250, 98), 1)
-        hero5_rect = pygame.draw.rect(self.background, LINECOLOR, (1050, 10, 250, 98), 1)
-        self.hero_rect = (hero1_rect, hero2_rect, hero3_rect, hero4_rect, hero5_rect)
-
-        pygame.draw.rect(self.background, LINECOLOR, (10,   120, 405, 500), 1)
-        pygame.draw.rect(self.background, LINECOLOR, (425,  120, 315, 670), 1)
-        pygame.draw.rect(self.background, LINECOLOR, (750,  120, 315, 670), 1)
-        pygame.draw.rect(self.background, LINECOLOR, (1075, 120, 315, 670), 1)
+        self._init_buttons()
+        self._init_boxes()
 
     def _init_buttons(self):
         bg_width = self.background.get_width()
@@ -67,54 +55,30 @@ class PartyScreen(object):
         button_w = screens.sprites.ButtonSprite(80, 30, (bg_width - 90, 80), "Next", pygame.K_w)
         self.buttons = (button_c, button_q, button_w)
 
+    def _init_boxes(self):
+        self.hero_boxes = []
+        for index, hero in enumerate(self.party):
+            self.hero_boxes.append(HeroBox((10 + index * 260, 10), hero))
+
+        pygame.draw.rect(self.background, LINECOLOR, (10,   120, 405, 500), 1)
+        pygame.draw.rect(self.background, LINECOLOR, (425,  120, 315, 670), 1)
+        pygame.draw.rect(self.background, LINECOLOR, (750,  120, 315, 670), 1)
+        pygame.draw.rect(self.background, LINECOLOR, (1075, 120, 315, 670), 1)
+
     def handle_view(self):
         """
-        self.background bij button.draw()? moet dit niet self.screen zijn? ik snap nog niet de voorwaarden.
-        bij de rest heb overal self.screen gedaan, maar blijkbaar kan het ook anders.
+        ...
         """
-
-        # todo, aparte classen voor de verschillende onderdelen
-
         self.screen.blit(self.background, (0, 0))
 
         for button in self.buttons:
-            button.draw(self.screen, self.key_input)
+            button.draw(self.background, self.key_input)
 
         cur_hero = self.party[self.hc]
 
-        pygame.draw.rect(self.screen, HEROCOLOR, self.hero_rect[self.hc], 0)
-        pygame.draw.rect(self.screen, LINECOLOR, self.hero_rect[self.hc], 1)
-
-        for index, hero in enumerate(self.party):
-
-            face = pygame.image.load(hero.FAC)
-            face_rect = self.screen.blit(face, (11 + index * 260, 11))
-
-            name = self.largefont.render(hero.NAM, True, FONTCOLOR)
-            name_rect = self.screen.blit(name, (121 + index * 260, 14))
-
-            level = self.normalfont.render("Level: {:12}".format(hero.lev.qty), True, FONTCOLOR)
-            level_rect = self.screen.blit(level, (121 + index * 260, 46))
-
-            hitpoints = self.normalfont.render(
-                                        "HitPoints: {:5}{} {}".format(hero.cur_hp, "/", hero.max_hp), True, FONTCOLOR)
-            hitpoints_rect = self.screen.blit(hitpoints, (121 + index * 260, 66))
-
-            # health bars ##########
-            full_hp = 135
-            curr_hp = (full_hp / hero.max_hp) * hero.cur_hp
-            color = HPCOLORFULL
-            if hero.lev.cur < hero.lev.qty:
-                color = HPCOLORHIGH
-            if hero.sta.cur < hero.sta.qty:
-                color = HPCOLORNORM
-            if hero.edu.cur < hero.edu.qty:
-                color = HPCOLORCRIT
-            if hero.edu.cur < hero.edu.qty and hero.sta.cur > 0:
-                color = HPCOLORLOW
-            pygame.draw.rect(self.screen, color, (121 + index * 260, 91, curr_hp, 13), 0)
-            pygame.draw.rect(self.screen, LINECOLOR, (121 + index * 260, 91, full_hp, 13), 1)
-            ########################
+        for hero_box in self.hero_boxes:
+            hero_box.draw(self.background)
+            hero_box.select(cur_hero)
 
         name2 = self.largefont.render(cur_hero.NAM, True, FONTCOLOR)
         name2_rect = self.screen.blit(name2, (300, 300))
@@ -157,6 +121,8 @@ class PartyScreen(object):
             self._previous()
         elif event.key == pygame.K_w:
             self._next()
+        # elif event.key == pygame.K_m:
+        #     self.party[0].lev.qty += 1
 
     def _previous(self):
         self.hc -= 1
@@ -167,3 +133,72 @@ class PartyScreen(object):
         self.hc += 1
         if self.hc > len(self.party) - 1:
             self.hc = 0
+
+
+class HeroBox(object):
+    """
+    ...
+    """
+    def __init__(self, position, hero):
+        self.surface = pygame.Surface((250, 98))
+        self.surface.fill(BACKGROUNDCOLOR)
+        self.surface = self.surface.convert()
+        self.rect = self.surface.get_rect()
+        self.rect.topleft = position
+
+        self.background = pygame.Surface((250, 98))
+        self.background.fill(BACKGROUNDCOLOR)
+        self.background = self.background.convert()
+
+        self.hero = hero
+
+    def _update(self):
+        largefont = pygame.font.SysFont(FONT, LARGEFONTSIZE)
+        normalfont = pygame.font.SysFont(FONT, NORMALFONTSIZE)
+
+        self.face = pygame.image.load(self.hero.FAC)
+        self.name = largefont.render(self.hero.NAM, True, FONTCOLOR)
+        self.level = normalfont.render("Level: {:12}".format(self.hero.lev.qty), True, FONTCOLOR)
+        self.hitpoints = normalfont.render(
+                                "HitPoints: {:5}{} {}".format(self.hero.cur_hp, "/", self.hero.max_hp), True, FONTCOLOR)
+        # health bars ##########
+        self.full_hp = 135
+        self.curr_hp = (self.full_hp / self.hero.max_hp) * self.hero.cur_hp
+        self.color = HPCOLORFULL
+        if self.hero.lev.cur < self.hero.lev.qty:
+            self.color = HPCOLORHIGH
+        if self.hero.sta.cur < self.hero.sta.qty:
+            self.color = HPCOLORNORM
+        if self.hero.edu.cur < self.hero.edu.qty:
+            self.color = HPCOLORCRIT
+        if self.hero.edu.cur < self.hero.edu.qty and self.hero.sta.cur > 0:
+            self.color = HPCOLORLOW
+        ########################
+
+    def draw(self, screen):
+        """
+        ...
+        :param screen:
+        """
+        self._update()
+
+        self.surface.blit(self.background, (0, 0))
+        pygame.draw.rect(self.surface, LINECOLOR, (0, 0, 250, 98), 1)
+        self.surface.blit(self.face, (1, 1))
+        self.surface.blit(self.name, (111, 4))
+        self.surface.blit(self.level, (111, 36))
+        self.surface.blit(self.hitpoints, (111, 56))
+        pygame.draw.rect(self.surface, self.color, (111, 81, self.curr_hp, 13), 0)
+        pygame.draw.rect(self.surface, LINECOLOR, (111, 81, self.full_hp, 13), 1)
+
+        screen.blit(self.surface, self.rect.topleft)
+
+    def select(self, hero):
+        """
+        ...
+        :param hero:
+        """
+        if hero == self.hero:
+            self.background.fill(HEROCOLOR)
+        else:
+            self.background.fill(BACKGROUNDCOLOR)
