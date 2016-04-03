12dccf1656008a108ea8ec90d27fa7d04eb8d119
Thomas M. Hesselink <mail@t64.nl>
2016-04-03 02:14:00 +0200

    voortgang shop

---
+++ screens/shop/buybox.py
@@ -0,0 +1,40 @@
+
+"""
+class: BuyBox
+"""
+
+import pygame
+
+COLORKEY = pygame.Color("purple")
+LINECOLOR = pygame.Color("black")
+
+FONTCOLOR = pygame.Color("white")
+FONT = 'impact'
+LARGEFONTSIZE = 25
+NORMALFONTSIZE = 15
+
+
+class BuyBox(object):
+    """
+    ...
+    """
+    def __init__(self, x, y, width, height):
+        self.surface = pygame.Surface((width, height))
+        self.surface.fill(COLORKEY)
+        self.surface.set_colorkey(COLORKEY)
+        self.surface = self.surface.convert()
+
+        self.rect = self.surface.get_rect()
+        self.rect.topleft = x, y
+
+        self.largefont = pygame.font.SysFont(FONT, LARGEFONTSIZE)
+        self.normalfont = pygame.font.SysFont(FONT, NORMALFONTSIZE)
+
+        pygame.draw.rect(self.surface, LINECOLOR, self.surface.get_rect(), 1)
+
+    def render(self, screen):
+        """
+        :param screen:
+        """
+
+        screen.blit(self.surface, self.rect.topleft)
--- screens/shop/display.py
+++ screens/shop/display.py
@@ -5,10 +5,23 @@ class: Display
 
 import pygame
 
+import keys
+import screens.shop.buybox
+import screens.shop.sellbox
 import statemachine
 
 BACKGROUNDCOLOR = pygame.Color("black")
 BACKGROUNDSPRITE = 'resources/sprites/parchment.png'
+
+BUYBOXWIDTH = 1/4
+BUYBOXHEIGHT = 2/3
+BUYBOXPOSX = 1/3
+BUYBOXPOSY = 1/6
+
+SELLBOXWIDTH = 1/4      # van het scherm
+SELLBOXHEIGHT = 2/3
+SELLBOXPOSX = 2/3       # x op 2/3 van het scherm
+SELLBOXPOSY = 1/6
 
 
 class Display(object):
@@ -18,27 +31,63 @@ class Display(object):
     def __init__(self):
         self.screen = pygame.display.get_surface()
         self.name = statemachine.States.Shop
-
         self.background = pygame.image.load(BACKGROUNDSPRITE).convert_alpha()
         self.background = pygame.transform.scale(self.background, self.screen.get_size())
-
         self.screen.fill(BACKGROUNDCOLOR)
         self.screen.blit(self.background, (0, 0))
 
+        self._init_boxes()
+
+    def _init_boxes(self):
+        width = self.screen.get_width() * SELLBOXWIDTH
+        height = self.screen.get_height() * SELLBOXHEIGHT
+        x = self.screen.get_width() * SELLBOXPOSX
+        y = self.screen.get_height() * SELLBOXPOSY
+        self.sellbox = screens.shop.sellbox.SellBox(x, y, width, height)
+        width = self.screen.get_width() * BUYBOXWIDTH
+        height = self.screen.get_height() * BUYBOXHEIGHT
+        x = self.screen.get_width() * BUYBOXPOSX
+        y = self.screen.get_height() * BUYBOXPOSY
+        self.buybox = screens.shop  .buybox.BuyBox(x, y, width, height)
+
     def on_enter(self):
+        """
+        Wanneer deze state op de stack komt, voer dit uit.
+        Op dit moment nog niets echts.
+        """
         pass
 
     def on_exit(self):
+        """
+        Wanneer deze state onder een andere state van de stack komt, voer dit uit.
+        Op dit moment nog niets echts.
+        """
         pass
 
     def single_input(self, event):
+        """
+        Handelt de muis en keyboard input af.
+        :param event: pygame.event.get()
+        """
         pass
 
     def multi_input(self, key_input, mouse_pos, dt):
+        """
+        :param key_input: pygame.key.get_pressed()
+        :param mouse_pos: pygame.mouse.get_pos()
+        :param dt: self.clock.tick(FPS)/1000.0
+        """
         pass
 
     def update(self, dt):
+        """
+        :param dt: self.clock.tick(FPS)/1000.0
+        """
         pass
 
     def render(self):
-        pass
+        """
+        Teken
+        """
+        self.buybox.render(self.screen)
+        self.sellbox.render(self.screen)
