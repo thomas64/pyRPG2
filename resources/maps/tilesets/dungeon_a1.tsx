097511f5e8c879ca640034dacab8ab79064ffc74
Thomas M. Hesselink <mail@t64.nl>
2016-05-11 00:36:15 +0200

    voortzet rules voor dungeon.

---
+++ resources/maps/tilesets/dungeon_a1.tsx
@@ -0,0 +1,144 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="dungeon_a1" tilewidth="16" tileheight="16" tilecount="768" columns="32">
+ <image source="dungeon_a1.png" width="512" height="384"/>
+ <tile id="386">
+  <animation>
+   <frame tileid="386" duration="300"/>
+   <frame tileid="390" duration="300"/>
+   <frame tileid="394" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="387">
+  <animation>
+   <frame tileid="387" duration="300"/>
+   <frame tileid="391" duration="300"/>
+   <frame tileid="395" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="418">
+  <animation>
+   <frame tileid="418" duration="300"/>
+   <frame tileid="422" duration="300"/>
+   <frame tileid="426" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="419">
+  <animation>
+   <frame tileid="419" duration="300"/>
+   <frame tileid="423" duration="300"/>
+   <frame tileid="427" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="448">
+  <animation>
+   <frame tileid="448" duration="300"/>
+   <frame tileid="452" duration="300"/>
+   <frame tileid="456" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="449">
+  <animation>
+   <frame tileid="449" duration="300"/>
+   <frame tileid="453" duration="300"/>
+   <frame tileid="457" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="450">
+  <animation>
+   <frame tileid="450" duration="300"/>
+   <frame tileid="454" duration="300"/>
+   <frame tileid="458" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="451">
+  <animation>
+   <frame tileid="451" duration="300"/>
+   <frame tileid="455" duration="300"/>
+   <frame tileid="459" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="480">
+  <animation>
+   <frame tileid="480" duration="300"/>
+   <frame tileid="484" duration="300"/>
+   <frame tileid="488" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="481">
+  <animation>
+   <frame tileid="481" duration="300"/>
+   <frame tileid="485" duration="300"/>
+   <frame tileid="489" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="482">
+  <animation>
+   <frame tileid="482" duration="300"/>
+   <frame tileid="486" duration="300"/>
+   <frame tileid="490" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="483">
+  <animation>
+   <frame tileid="483" duration="300"/>
+   <frame tileid="487" duration="300"/>
+   <frame tileid="491" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="512">
+  <animation>
+   <frame tileid="512" duration="300"/>
+   <frame tileid="516" duration="300"/>
+   <frame tileid="520" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="513">
+  <animation>
+   <frame tileid="513" duration="300"/>
+   <frame tileid="517" duration="300"/>
+   <frame tileid="521" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="514">
+  <animation>
+   <frame tileid="514" duration="300"/>
+   <frame tileid="518" duration="300"/>
+   <frame tileid="522" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="515">
+  <animation>
+   <frame tileid="515" duration="300"/>
+   <frame tileid="519" duration="300"/>
+   <frame tileid="523" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="544">
+  <animation>
+   <frame tileid="544" duration="300"/>
+   <frame tileid="548" duration="300"/>
+   <frame tileid="552" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="545">
+  <animation>
+   <frame tileid="545" duration="300"/>
+   <frame tileid="549" duration="300"/>
+   <frame tileid="553" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="546">
+  <animation>
+   <frame tileid="546" duration="300"/>
+   <frame tileid="550" duration="300"/>
+   <frame tileid="554" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="547">
+  <animation>
+   <frame tileid="547" duration="300"/>
+   <frame tileid="551" duration="300"/>
+   <frame tileid="555" duration="300"/>
+  </animation>
+ </tile>
+</tileset>
