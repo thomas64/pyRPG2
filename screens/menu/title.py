d234e64a978ddd67cc3b5f778f54bd16674c5dc6
Thomas M. Hesselink <mail@t64.nl>
2016-06-07 23:11:43 +0200

    versie PreAlpa v.0.010. shop geimplementeerd.

--- screens/menu/title.py
+++ screens/menu/title.py
@@ -19,7 +19,7 @@ SUBFONTCOLOR = pygame.Color("gray36")
 SUBPOSX = 200
 SUBPOSY = 160
 
-VERSIONTEXT = "PreAlpha v0.009"
+VERSIONTEXT = "PreAlpha v0.010"
 VERSIONFONT = 'verdana'
 VERSIONFONTSIZE = 12
 VERSIONFONTCOLOR = pygame.Color("white")
